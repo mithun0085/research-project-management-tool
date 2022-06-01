42a41b9f3d665d28d88ee46f44c42ff711c95ea2
Mudannayake M.A.P.H.N <it20248994@my.sliit.lk>
2022-06-01 11:26:26 +0530

    Update colours headde4r footer

--- frontend/src/components/footer/styles/footer.js
+++ frontend/src/components/footer/styles/footer.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 80px 60px;
-  background: black;
+  background: #282A35;
   @media (max-width: 1000px) {
     padding: 70px 30px;
   }
